9e7ca2d8a752ea11beb81617d5085f4e56970e13
Cavell Blood <me@cavellblood.com>
2011-11-07 09:19:57 -0800

    Added new features
    You can now choose which category your event would be listed under.
    Also began work on a date picker.

--- template-files/sponsors/add-event.php
+++ template-files/sponsors/add-event.php
@@ -1,6 +1,7 @@
 {embed="includes/_doc-top" 
   channel="sponsors"
-  title="Sponsorship Program | Create a new event"}
+  title="Sponsorship Program | Create a new event"
+  add="datePicker"}
 <div class="body">
   <div class="heading clearafter">
     <h1>Create a new event</h1>
@@ -135,7 +136,6 @@
         <th scope="row"><label for="field_id_35">* Description</label></th>
         <td>
           <textarea id="field_id_35" class="input" name="field_id_35" dir="ltr" cols="36" rows="8"></textarea><br>
-          <input type="hidden" name="field_ft_35" value="textile">
           <p class="instructions">Should include event description, contact information, registration instructions, etc.</p>
             <p class="instructions">Follow these <strong><a href="#formatting-tips" class="formatting-tips">formatting tips</a></strong> to make text bold, italic, or as a bulleted list.</p>
             <p id="formatting-tips" class="instructions">
@@ -159,6 +159,33 @@
         </td>
       </tr>
       <tr>
+        <th scope="row">
+          <label for="field_id_35">* Categories</label>
+        </th>
+        <td>
+          <br>
+          <p class="instructions"><strong>Please select at least one category for your event.</strong></p>
+          <input type="checkbox" name="category[]" value="374" /> <span>Finding Peace</span><br />
+          <input type="checkbox" name="category[]" value="371" /> <span>Fitness</span><br />
+          <input type="checkbox" name="category[]" value="376" /> <span>Health Expo</span><br />
+          <input type="checkbox" name="category[]" value="369" /> <span>Heart Disease Prevention</span><br />
+          <input type="checkbox" name="category[]" value="372" /> <span>Improving Mental Performance</span><br />
+          <input type="checkbox" name="category[]" value="367" /> <span>Natural Remedies</span><br />
+          <input type="checkbox" name="category[]" value="375" /> <span>Overcoming Depression</span><br />
+          <input type="checkbox" name="category[]" value="373" /> <span>Reducing Stress</span><br />
+          <input type="checkbox" name="category[]" value="365" /> <span>Reversing Diabetes</span><br />
+          <input type="checkbox" name="category[]" value="370" /> <span>Stop Smoking</span><br />
+          <input type="checkbox" name="category[]" value="366" /> <span>Vegetarian Cooking</span><br />
+          <input type="checkbox" name="category[]" value="368" /> <span>Weight Management</span><br />
+        </td>
+      </tr>
+      <tr class="hide">
+        <td>
+          <input name="start-date" id="start-date" class="date-pick" />
+          <input name="end-date" id="end-date" class="date-pick" />
+        </td>
+      </tr>
+      <tr>
         <th></th>
         <td>
           <div class="button-wrap">
@@ -174,8 +201,9 @@
   <div class="right sidebar">
     <div class="bar">Add Events</div>
       <p>To view or edit an event, click <a href="/sponsors/edit-event">here</a>.</p>
-      <p><strong>IMPORTANT</strong>: Only approved {site_name} events may be added. If your event type is not listed <a href="/sponsors/resources">here</a>, please email <a href="mailto:club@newstart.com">club@newstart.com</a> or call 530-422-7993 before adding your event.</p>
+      <p><strong>IMPORTANT:</strong> Please create a separate entry for each day of an event that does not fall on consecutive days.</p>
+      <p>Only approved {site_name} events may be added. If your event type is not listed <a href="/sponsors/resources">here</a>, please email <a href="mailto:club@newstart.com">club@newstart.com</a> or call 530-422-7993 before adding your event.</p>
   </div>
   </div><!-- /.grid23 -->
 </div><!-- /.body -->
-{embed="includes/_doc_bottom" script_add="jquery.maskedinput-1.3.min|sponsors-masking|sponsors"}
+{embed="includes/_doc_bottom" script_add="jquery.date.min|jquery.datePicker.min|jquery.maskedinput-1.3.min|sponsors-masking|sponsors"}
