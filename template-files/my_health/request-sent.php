851888375ffe4b9b5fae4bca835e2a56e3391a7e
Cavell L. Blood <cavellblood@Cavells-MBP.local>
2011-11-26 21:46:32 -0800

    Updated HTML Email template

--- template-files/my_health/request-sent.php
+++ template-files/my_health/request-sent.php
@@ -22,17 +22,58 @@
   
   $subject = "Tell me more about your NEWSTART® Lifestyle Program";
   
-  $message = "
-<p><strong>Name</strong>: $memberName</p>
-<p><strong>Age</strong>: $memberAge</p>
-<p><strong>Address</strong>: $memberAddress</p>
-<p><strong>Phone</strong>: $memberPhone</p>
-<p><strong>Email</strong>: $memberEmail</p>
-<p><strong>My Health Score</strong>: $memberHS</p>
-<p><strong>Message</strong>: $customMessage</p>
-
---------------------------- <br />
-Sent from <a href='www.newstartclub.com'>www.newstartclub.com</a>";
+  $message = '
+  <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+  <html>
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=610">
+  <title>Information Request</title>
+  <style>
+  a {color:#87A621;}
+  </style>
+  </head>
+    <body style="background: url(http://newstartclub.com/assets/css/images/background-gradient-texture.png) #A7C7EF repeat-x;" leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" bgcolor="#A7C7EF" >
+      <table width="100%" cellpadding="30" cellspacing="0" class="backgroundTable">
+        <tr>
+          <td valign="top" align="center">
+            <table width="550" cellpadding="0" cellspacing="0">
+              <tr>
+                <td style="border-top:0px solid #333333;">
+                <center><a href="http://newstartclub.com/"><IMG SRC="http://newstartclub.com/assets/images/email/invites/header-email.jpg" BORDER="0" title="NEWSTART&reg; Lifestyle Club"  alt="NEWSTART&reg; Lifestyle Club" align="center" style="border-top-left-radius: 15px; -moz-border-radius-topleft: 15px; -webkit-border-top-left-radius: 15px; -khtml-border-top-left-radius: 15px; border-top-right-radius: 15px; -moz-border-radius-topright: 15px; -webkit-border-top-right-radius: 15px; -khtml-border-top-right-radius: 15px; -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.35);-moz-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25);" /></a></center>
+              </td>
+              </tr>
+            </table>
+            <table width="550" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+              <td style="background: url(http://newstartclub.com/assets/images/email/newsletter/nav-texture.png) center center #000000; color:#FFFFFF; height: 60px; text-align:right; padding-right: 30px; -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.35);-moz-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25);">
+                <span style="font-size:30px;color:#FFFFFF;font-family:\'Helvetica Neue\', Arial, Helvetica, sans-serif;">Information Request</span>
+              </td>
+            </table>
+            <table width="550" cellpadding="0" cellspacing="0">
+              <tr>
+                <td bgcolor="#FFFFFF" valign="top" style="font-size:16px;color:#000000;line-height:150%;font-family:\'Helvetica Neue\', Arial, Helvetica, sans-serif; border-bottom-left-radius: 15px; -moz-border-radius-bottomleft: 15px; -webkit-border-bottom-left-radius: 15px; -khtml-border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; -moz-border-radius-bottomright: 15px; -webkit-border-bottom-right-radius: 15px; -khtml-border-bottom-right-radius: 15px; -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.35);-moz-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25); padding:30px;">
+                  <p><strong>Name</strong>: '. $memberName .'</p>
+                  <p><strong>Age</strong>: '. $memberAge .'</p>
+                  <p><strong>Address</strong>: '. $memberAddress .'</p>
+                  <p><strong>Phone</strong>: '. $memberPhone .'</p>
+                  <p><strong>Email</strong>: '. $memberEmail .'</p>
+                  <p><strong>My Health Score</strong>: '. $memberHS .'</p>
+                  <p><strong>Message</strong>: '. $customMessage .'</p>
+                </td>
+              </tr>
+            </table>
+            <table width="550" cellpadding="0" cellspacing="0">
+              <tr>
+                <td style="background-color:transparent; text-align:center; padding-top:10px;" valign="top">
+                  
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </body>
+  </html>';
   
   // To send HTML mail, the Content-type header must be set
 	$headers  = 'MIME-Version: 1.0' . "\n";
@@ -69,4 +110,20 @@ Sent from <a href='www.newstartclub.com'>www.newstartclub.com</a>";
     </div><!--/.sidebar-->
   </div><!--/.grid23-->
 </div><!-- /.body -->
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('.vimeoBadge .clip > a').attr('rel', 'prettyPhoto');
+    $('.vimeoBadge a img').attr('alt', 'NEWSTART Now');
+    $(".clip").each(function(){
+          alt = $(this).find("img").attr("alt");
+          title = $(this).find(".caption a").text();
+          $(this).find("> a").attr("title", title);
+      });
+    $(".vimeoBadge a[rel^='prettyPhoto']")
+    .prettyPhoto({
+      theme:'dark_rounded',
+      animationSpeed: 'slow'
+    });
+  });
+</script>
 {embed="includes/_doc_bottom" script_add="jquery.prettyPhoto_3.0"}
--- template-files/partners/application-submitted.php
+++ template-files/partners/application-submitted.php
@@ -1,7 +1,90 @@
 <?php
+
+function send_emails($from_name, $from_email, $subject, $custom_message)
+{
+  // To send HTML mail, the Content-type header must be set
+  $headers  = 'MIME-Version: 1.0' . "\n";
+  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\n";
+  $headers .= 'From: '. $from_name .' <'. $from_email .'>' . "\r\n";
+
+    // message
+    $message = '
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+    <html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=610">
+    <title>Email Members Template</title>
+    </head>
+      <body style="background: url(http://newstartclub.com/assets/css/images/background-gradient-texture.png) #A7C7EF repeat-x;" leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" bgcolor="#A7C7EF" >
+        <table width="100%" cellpadding="30" cellspacing="0" class="backgroundTable">
+          <tr>
+            <td valign="top" align="center">
+              <table width="550" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td style="border-top:0px solid #333333;">
+                  <center><a href="http://newstartclub.com/"><IMG SRC="http://newstartclub.com/assets/images/email/invites/header-email.jpg" BORDER="0" title="NEWSTART&reg; Lifestyle Club"  alt="NEWSTART&reg; Lifestyle Club" align="center" style="border-top-left-radius: 15px; -moz-border-radius-topleft: 15px; -webkit-border-top-left-radius: 15px; -khtml-border-top-left-radius: 15px; border-top-right-radius: 15px; -moz-border-radius-topright: 15px; -webkit-border-top-right-radius: 15px; -khtml-border-top-right-radius: 15px; -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.35);-moz-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25);" /></a></center>
+                </td>
+                </tr>
+              </table>
+              <table width="550" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+                <td style="background: url(http://newstartclub.com/assets/images/email/newsletter/nav-texture.png) center center #000000; color:#FFFFFF; height: 60px; text-align:right; padding-right: 30px; -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.35);-moz-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25);">
+                  <span style="font-size:30px;color:#FFFFFF;font-family:\'Helvetica Neue\', Arial, Helvetica, sans-serif;">Partnership Application</span>
+                </td>
+              </table>
+              <table width="550" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td bgcolor="#FFFFFF" valign="top" style="font-size:16px;color:#000000;line-height:150%;font-family:\'Helvetica Neue\', Arial, Helvetica, sans-serif; border-bottom-left-radius: 15px; -moz-border-radius-bottomleft: 15px; -webkit-border-bottom-left-radius: 15px; -khtml-border-bottom-left-radius: 15px; border-bottom-right-radius: 15px; -moz-border-radius-bottomright: 15px; -webkit-border-bottom-right-radius: 15px; -khtml-border-bottom-right-radius: 15px; -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.35);-moz-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25); padding:30px;">';
+          $message .= $custom_message;
+          $message .= '
+                  </td>
+                </tr>
+              </table>
+              <table width="550" cellpadding="0" cellspacing="0">
+                <tr>
+                  <td style="background-color:transparent; text-align:center; padding-top:10px;" valign="top">
+                    <span style="font-size:10px;color:#FFFFFF;font-family:verdana;"></span>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </body>
+    </html>';
   
+    // Mail it
+    mail("cavellblood@me.com, club@newstart.com", $subject, stripslashes($message), $headers);
+
+}
+
+function show_empty()
+{
+  print '
+    <h1>Your application is empty!</h1> 
+    <p>It appears that you&rsquo;ve reached this page without submitting the Partner Application.</p>
+    <p class="button-wrap">
+      <a href="/partners/apply" class="super green button"><span>Apply</span></a>
+    </p>
+  ';
+}
+
+function show_done()
+{
+
+  print '
+    <h1>Application Sent!</h1>
+    <p>Thank you for submitting a partnership application!</p>
+    <p>A {site_name} representative will contact you after your application has been reviewed.</p>
+    <p class="button-wrap">
+      <a href="/" class="super green button"><span>Home</span></a>
+    </p>
+  ';
+}
+
 if (isset($_POST["name"])) {
   $name = $_POST["name"];
+  $email = $_POST["email"];
   $credentials = $_POST["credentials"];
   $address = $_POST["address"];
   $city = $_POST["city"];
@@ -14,6 +97,7 @@ if (isset($_POST["name"])) {
   
   $message = "
     <p><strong>Name:</strong> $name</p>
+    <p><strong>E-mail:</strong> $email</p>
     <p><strong>Credentials:</strong> $credentials</p>
     <p><strong>Address:</strong> $address</p>
     <p><strong>City:</strong> $city</p>
@@ -24,9 +108,10 @@ if (isset($_POST["name"])) {
     <p><strong>Picture:</strong> $picture</p>
     <p><strong>Affiliation:</strong> $affiliation</p>
   ";
+  send_emails($name, $email, 'Partnership Application', $message);
 }
 
-if (isset($_FILES["partnerPicture"])) {
+if ($_FILES["partnerPicture"]["name"] != "") {
   if (
       (
         ($_FILES["partnerPicture"]["type"] == "image/jpeg") ||
@@ -46,21 +131,21 @@ if (isset($_FILES["partnerPicture"])) {
       {
       if (file_exists("partners-upload/" . $_FILES["partnerPicture"]["name"]))
         {
-        echo $_FILES["partnerPicture"]["name"] . " already exists. ";
+        echo "<!-- ". $_FILES["partnerPicture"]["name"] . " already exists. -->";
         }
       else
         {
         move_uploaded_file($_FILES["partnerPicture"]["tmp_name"],
         "partners-upload/" . $_FILES["partnerPicture"]["name"]);
-        echo "Stored in: " . "upload/" . $_FILES["partnerPicture"]["name"];
         }
       }
     }
   else
     {
-    echo "Invalid file";
+    echo "<!-- Invalid file -->";
     }
 }
+
 ?>
 
 <!doctype html>
@@ -95,20 +180,41 @@ function fileModTime($filename) {
 <body>
 <div class="container">
   <div class="body">
-    <?php if (isset($_POST['name'])) { ?>
-      <h1>Application Sent</h1>
-      <p>Thank you for submitting a partnership application!</p>
-      <p>A {site_name} representative will contact you after your application has been reviewed.</p>
-      <p class="button-wrap">
-        <a href="/" class="super green button"><span>Home</span></a>
-      </p>
-    <?php } else { ?>
-      <h1>Your application is empty!</h1> 
-      <p>It appears that you've reached this page without submitting the Sponsor Application.</p>
-      <p class="button-wrap">
-        <a href="/partners/apply" class="super green button"><span>Apply</span></a>
-      </p>
-    <?php } ?>
+    <?php 
+      if (isset($_POST['name'])) {
+        if (($_FILES["partnerPicture"]["name"] != "") && 
+            (
+              ($_FILES["partnerPicture"]["type"] == "image/jpeg") ||
+              ($_FILES["partnerPicture"]["type"] == "image/pjpeg") ||
+              ($_FILES["partnerPicture"]["type"] == "image/png") ||
+              ($_FILES["partnerPicture"]["type"] == "image/tiff") ||
+              ($_FILES["partnerPicture"]["type"] == "image/bmp")
+            ) 
+            && ($_FILES["partnerPicture"]["size"] < 2000000)
+          )
+          {
+            show_done();
+          }
+        else
+          {
+            print '
+              <h1>Invalid File</h1>
+              <p>The file you uploaded for your picture could not be recognized or was over 2MB. Please upload one of the following file types: .jpg, .bmp, .tiff, .png.</p>
+              <form action="" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="name" class="input" id="name" size="25" value="'. $name .'" />
+                <input type="file" name="partnerPicture" id="partnerPicture" />
+                <div class="button-wrap">
+                  <button type="submit" class="super green button"><span>Submit</span></button>
+                </div>
+              </form>
+            ';
+          }
+        }
+      else
+        {
+          show_empty();
+        }
+    ?>
   </div>
 </div>
 </body>
