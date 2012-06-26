f00de40a82ded1534c63d4c65599b10a292d363b
Cavell Blood <cavellblood@me.com>
2012-06-26 08:28:57 -0700

    Removed old files

--- template-files/includes/_share.php
+++
@@ -1,7 +0,0 @@
-<div class="bar">Share</div>
-<a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="newstartclub" data-related="weimarmedia">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-<div id="fb-root"></div>
-<script src="http://connect.facebook.net/en_US/all.js#appId=120374141384910&amp;xfbml=1"></script>
-<fb:like href="{exp:weblog:entries weblog='{embed:channel}' url_title='{segment_3}'}{title_permalink='{embed:channel}/detail'}{/exp:weblog:entries}" layout="button_count" width="180" show_faces="false" font=""></fb:like>
-<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
-<fb:send href="{exp:weblog:entries weblog='{embed:channel}' url_title='{segment_3}'}{title_permalink='{embed:channel}/detail'}{/exp:weblog:entries}" font=""></fb:send>
