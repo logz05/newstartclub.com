240245fa103589f0372d6e4e7b1aba475b046ab0
Cavell Blood <cavellblood@me.com>
2012-10-23 10:25:49 -0700

    Fixed indenting issues

--- template-files/news/search.php
+++ template-files/news/search.php
@@ -11,47 +11,50 @@ require_once ( 'utilities.php' );
 ?>
 {embed="embeds/_rss-feed"}
 <div class="heading clearfix">
-		<h1>Search Results</h1>
+	<h1>Search Results</h1>
+</div>
+<div class="post">
+	<p>Your search for <strong>&ldquo;{exp:search:keywords}&rdquo;</strong> found {exp:search:total_results}{total_results}{/exp:search:total_results} result{if "{exp:search:total_results}" != 1}s{/if}.</p>
+</div>
+<div class="grid23 clearfix">
+	<div class="main left">
+{exp:search:search_results}
+	{if count == 1}
+		<ul class="listing">
+	{/if}
+			<li class="entry {channel_short_name}">
+				{if channel_short_name == "resources"}<h2 data-icon="d">{/if}
+				{if channel_short_name == "services" }<h2 data-icon="a">{/if}
+				{if channel_short_name == "events"   }<h2 data-icon="e">{/if}
+				{if channel_short_name == "locations"}<h2 data-icon="f">{/if}
+				{if channel_short_name == "recipes"  }<h2 data-icon="g">{/if}
+				{if channel_short_name == "questions"}<h2 data-icon="i">{/if}
+				{if channel_short_name == "deals"    }<h2 data-icon="n">{/if}
+					
+					{if channel_short_name == "deals"}
+						<a href="{path='deals/detail/{categories show_group="24"}{category_url_title}{/categories}'}">{exp:eehive_hacksaw chars="48"}{title}{/exp:eehive_hacksaw}</a>
+					{if:elseif channel_short_name == "questions"}
+						<a href="{path='questions/detail/{url_title}'}">{exp:eehive_hacksaw chars="48" append="&hellip;"}{question_question}{/exp:eehive_hacksaw}</a>
+					{if:else}
+						<a href="{path='{channel_short_name}/detail/{url_title}'}">{exp:eehive_hacksaw chars="48" append="&hellip;"}{title}{/exp:eehive_hacksaw}</a>
+					{/if}
+				</h2>
+				<div class="date">
+					<span class="timeago">{if entry_date > current_time}Upcoming{if:else}<?php echo distanceOfTimeInWords('{entry_date}', '{current_time}', true); ?>{/if}</span>
+					<span class="entry-date">{entry_date format="%D, %M %j, %Y	%g:%i%a %T"}</span>
+				</div>
+			</li>
+	{if count == total_results}
+		</ul>
+	{/if}
+	
+	{paginate}
+		<p class="pagination">{pagination_links}</p>
+	{/paginate}
+{/exp:search:search_results}
 	</div>
-	<div class="post">
-		<p>Your search for <strong>&ldquo;{exp:search:keywords}&rdquo;</strong> found {exp:search:total_results}{total_results}{/exp:search:total_results} result{if "{exp:search:total_results}" != 1}s{/if}.</p>
+	<div class="sidebar right">
+		{embed="news/_sidebar"}
 	</div>
-	<div class="grid23 clearfix">
-		<div class="main left">
-			<ul class="listing">
-			{exp:search:search_results}
-				<li class="entry {channel_short_name}">
-					{if channel_short_name == "resources"}<h2 data-icon="d">{/if}
-					{if channel_short_name == "services" }<h2 data-icon="a">{/if}
-					{if channel_short_name == "events"   }<h2 data-icon="e">{/if}
-					{if channel_short_name == "locations"}<h2 data-icon="f">{/if}
-					{if channel_short_name == "recipes"  }<h2 data-icon="g">{/if}
-					{if channel_short_name == "questions"}<h2 data-icon="i">{/if}
-					{if channel_short_name == "deals"    }<h2 data-icon="n">{/if}
-						
-						{if channel_short_name == "deals"}
-							<a href="{path='deals/detail/{categories show_group="24"}{category_url_title}{/categories}'}">{exp:eehive_hacksaw chars="48"}{title}{/exp:eehive_hacksaw}</a>
-						{if:elseif channel_short_name == "questions"}
-							<a href="{path='questions/detail/{url_title}'}">{exp:eehive_hacksaw chars="48" append="&hellip;"}{question_question}{/exp:eehive_hacksaw}</a>
-						{if:else}
-							<a href="{path='{channel_short_name}/detail/{url_title}'}">{exp:eehive_hacksaw chars="48" append="&hellip;"}{title}{/exp:eehive_hacksaw}</a>
-						{/if}
-					</h2>
-					<div class="date">
-						<span class="timeago">{if entry_date > current_time}Upcoming{if:else}<?php echo distanceOfTimeInWords('{entry_date}', '{current_time}', true); ?>{/if}</span>
-						<span class="entry-date">{entry_date format="%D, %M %j, %Y	%g:%i%a %T"}</span>
-					</div>
-				</li>
-			{/exp:search:search_results}
-			{if paginate}
-				<li class="pagination">
-					<p>{paginate}</p>
-				</li>
-			{/if}
-			</ul>
-		</div>
-		<div class="sidebar right">
-			{embed="news/_sidebar"}
-		</div>
-	</div>
+</div>
 {embed="embeds/_doc-bottom"}
--- template-files/sponsors/_apply-modal.php
+++ template-files/sponsors/_apply-modal.php
@@ -1,54 +1,54 @@
 {if logged_out}
 <div id="signin-modal-sponsor-apply" class="reveal-modal">
-  <div class="grid12 clearfix">
-    <div class="left">
-      <h2>Sign In</h2>
-      <h3>You must sign in to apply for sponsorship.</h3>
-      {exp:member:login_form error_page="members/error" return="/sponsors/apply"}
-      <table>
-        <tr>
-          <th scope="row"><label for="email">Email</label></th>
-          <td><input type="email" class="input" id="email" name="username" value="" size="23" autocapitalize="off" autocorrect="off" autocomplete="off"/></td>
-        </tr>
-        <tr>
-          <th scope="row"><label for="password">Password</label></th>
-          <td><input type="password" class="input" id="password" name="password" size="23" /></td>
-        </tr>
-        {if auto_login}<tr>
-          <th></th>
-          <td>
-            <p>
-              <input class="checkbox" type="checkbox" name="auto_login" value="1" /><span>Keep me logged in</span>
-            </p>
-          </td>
-        </tr>{/if}
-        <tr>
-          <th scope="row">&nbsp;</th>
-          <td>
-            <div class="button-wrap">
-              <button type="submit" class="super green button"><span>Sign In</span></button>
-              <a class="forgot-pass" href="/forgot-password">Forgot your password?</a>
-            </div>
-          </td>
-        </tr>
-      </table>
-      {/exp:member:login_form}
-    </div>
-    <div class="right">
-      <h2>Not a Member?</h2>
-      <h3>Here&rsquo;s some of the benefits:</h3>
-      <ul>
-        <li>Live streaming videos</li>
-        <li>Local seminars &amp; events</li>
-        <li>Expert health advice</li>
-        <li>Wellness tips &amp; tools</li>
-        <li>FREE membership</li>
-      </ul>
-      <p class="button-wrap">
-        <a href="/register" class="super red button"><span>Register</span></a>
-      </p>
-    </div>
-  </div>
-  <a class="close-modal close">&times;</a>
+	<div class="grid12 clearfix">
+		<div class="left">
+			<h2>Sign In</h2>
+			<h3>You must sign in to apply for sponsorship.</h3>
+			{exp:member:login_form error_page="members/error" return="/sponsors/apply"}
+			<table>
+				<tr>
+					<th scope="row"><label for="email">Email</label></th>
+					<td><input type="email" class="input" id="email" name="username" value="" size="23" autocapitalize="off" autocorrect="off" autocomplete="off"/></td>
+				</tr>
+				<tr>
+					<th scope="row"><label for="password">Password</label></th>
+					<td><input type="password" class="input" id="password" name="password" size="23" /></td>
+				</tr>
+				{if auto_login}<tr>
+					<th></th>
+					<td>
+						<p>
+							<input class="checkbox" type="checkbox" name="auto_login" value="1" /><span>Keep me logged in</span>
+						</p>
+					</td>
+				</tr>{/if}
+				<tr>
+					<th scope="row">&nbsp;</th>
+					<td>
+						<div class="button-wrap">
+							<button type="submit" class="super green button"><span>Sign In</span></button>
+							<a class="forgot-pass" href="{path='forgot-password'}">Forgot your password?</a>
+						</div>
+					</td>
+				</tr>
+			</table>
+			{/exp:member:login_form}
+		</div>
+		<div class="right">
+			<h2>Not a Member?</h2>
+			<h3>Here&rsquo;s some of the benefits:</h3>
+			<ul>
+				<li>Live streaming videos</li>
+				<li>Local seminars &amp; events</li>
+				<li>Expert health advice</li>
+				<li>Wellness tips &amp; tools</li>
+				<li>FREE membership</li>
+			</ul>
+			<p class="button-wrap">
+				<a href="{path='register'}" class="super red button"><span>Join</span></a>
+			</p>
+		</div>
+	</div>
+	<a class="close-modal close">&times;</a>
 </div>
 {/if}
