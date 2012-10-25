2ac329a648404c5bab10461cfdaa61f45a12cbf1
Cavell Blood <cavellblood@me.com>
2012-10-25 08:34:20 -0700

    Restored http: to the url in the footer

--- template-files/embeds/_doc-bottom.php
+++ template-files/embeds/_doc-bottom.php
@@ -1,6 +1,6 @@
 </div><!-- /.body -->
 <footer class="footer">
-	<p>A FREE community service of <a href="//newstart.com">NEWSTART&reg;</a>. &copy; 2010-{current_time format="%Y"}. All Rights Reserved.</p>
+	<p>A FREE community service of <a href="http://newstart.com">NEWSTART&reg;</a>. &copy; 2010-{current_time format="%Y"}. All Rights Reserved.</p>
 	<p>
 		<a href="{path='about/privacy-policy'}">Privacy Policy</a> |
 		<a href="{path='about/terms-of-use'}">Terms of Use</a> |
