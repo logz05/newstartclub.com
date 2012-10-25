61e81e290146a7f7154523212cecea5ccbfa3af9
Cavell Blood <cavellblood@me.com>
2012-10-25 08:41:17 -0700

    Updated to use plugin instead of PHP for redirection

--- template-files/event/index.php
+++ template-files/event/index.php
@@ -1,27 +1,15 @@
 {if segment_2 == "1562"}
 
-	{exp:channel:entries channel="events" entry_id="898" show_future_entries="yes"}
+	{exp:channel:entries channel="events" entry_id="898" show_future_entries="yes" show_expired="yes"}
 	
-		<?php
-		
-		header("Location: /events/detail/{url_title}"); /* Redirect browser */
-		
-		exit;
-		
-		?>
+		{exp:http_header status="301" location="{url_title_path='events/detail'}" terminate="yes"}
 		
 	{/exp:channel:entries}
 
 {/if}
 
-{exp:channel:entries channel="events" entry_id="{segment_2}" show_future_entries="yes"}
+{exp:channel:entries channel="events" entry_id="{segment_2}" show_future_entries="yes" show_expired="yes"}
 
-	<?php
-	
-	header("Location: /events/detail/{url_title}"); /* Redirect browser */
-	
-	exit;
-	
-	?>
+	{exp:http_header status="301" location="{url_title_path='events/detail'}" terminate="yes"}
 	
 {/exp:channel:entries}
