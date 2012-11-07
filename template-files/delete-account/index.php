d8ceda65afd6de6912066a98df6c332ef639d725
Cavell Blood <cavellblood@me.com>
2012-11-06 16:15:02 -0800

    Converted spaces to tabs

--- template-files/delete-account/index.php
+++ template-files/delete-account/index.php
@@ -1,42 +1,42 @@
 <!doctype html>
 <!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
-<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if IE 7 ]>		 <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>		 <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>		 <html lang="en" class="no-js ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 <head>
-  <meta charset="utf-8">
-  <meta name="apple-mobile-web-app-capable" content="yes" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=860;" />
+	<meta charset="utf-8">
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=860;" />
  
-  <title>Delete Account | {site_name}</title>
+	<title>Delete Account | {site_name}</title>
 
-  <link rel="stylesheet" href="{stylesheet='site/boilerplate'}" type="text/css" />
-  <link rel="stylesheet" href="{stylesheet='site/standalone'}" type="text/css" />
+	<link rel="stylesheet" href="{stylesheet='site/boilerplate'}" type="text/css" />
+	<link rel="stylesheet" href="{stylesheet='site/standalone'}" type="text/css" />
 </head>
 <body class="small">
-  <div class="body">
-    <h1>Delete Account</h1>
-    <p>This will delete all entries, posts, comments, and other content associated with your account.</p>
-    <p>WARNING: THIS ACTION CANNOT BE UNDONE!</p>
-  {exp:user:delete_form return="delete-feedback" form:class="clearfix" form:id="delete-account"} 
-    <h2>Delete account: {email}</h2>  
-    <table>
-      <tr>
-        <th scope="row"><label for="password">Your Password:</label></th>
-        <td>
-          <input type="password" class="input" name="password" />
-          <input type="hidden" class="hidden" name="delete-account-email" value="{email}" />
-          <input type="hidden" class="hidden" name="delete-account-name" value="{exp:user:stats}{member_first_name} {member_last_name}{/exp:user:stats}" />
-        </td>
-      </tr>
-    </table>
-    <p class="button-wrap">
-      <a href="/settings" class="super green button"><span>&laquo; Back to Settings</span></a>
-      <button type="submit" class="super red button"><span>Delete Account</span></button>
-    </p>
-  {/exp:user:delete_form}
-  </div>
+	<div class="body">
+		<h1>Delete Account</h1>
+		<p>This will delete all comments and other content associated with your account.</p>
+		<p>WARNING: THIS ACTION CANNOT BE UNDONE!</p>
+	{exp:user:delete_form return="delete-feedback" form:class="clearfix" form:id="delete-account"} 
+		<h2>Delete account: {email}</h2>	
+		<table>
+			<tr>
+				<th scope="row"><label for="password">Your Password:</label></th>
+				<td>
+					<input type="password" class="input" name="password" />
+					<input type="hidden" class="hidden" name="delete-account-email" value="{email}" />
+					<input type="hidden" class="hidden" name="delete-account-name" value="{exp:user:stats}{member_first_name} {member_last_name}{/exp:user:stats}" />
+				</td>
+			</tr>
+		</table>
+		<p class="button-wrap">
+			<a href="{path='settings'}" class="super green button"><span>&laquo; Back to Settings</span></a>
+			<button type="submit" class="super red button"><span>Delete Account</span></button>
+		</p>
+	{/exp:user:delete_form}
+	</div>
 </body>
 </html>
--- template-files/delete-feedback/index.php
+++ template-files/delete-feedback/index.php
@@ -10,7 +10,7 @@ $message = '<p><strong>From:</strong> '. $_SESSION['delete-account-name'] .'</p>
 $message .= '<p><strong>Email:</strong> '. $_SESSION['delete-account-email'] .'</p>';
 $message .= '<p><strong>Message:</strong> '. nl2br($reason) .'</p>';
 
-$headers  = 'MIME-Version: 1.0' . "\n";
+$headers	= 'MIME-Version: 1.0' . "\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\n";
 $headers .= 'From: '. $_SESSION['delete-account-name'] .' <'. $_SESSION['delete-account-email'] . '>'. "\r\n";
 
@@ -21,41 +21,41 @@ mail("club@newstart.com", stripslashes($subject), stripslashes($message), $heade
 
 <!doctype html>
 <!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
-<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if IE 7 ]>		 <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>		 <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>		 <html lang="en" class="no-js ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 <head>
-  <meta charset="utf-8">
-  <meta name="apple-mobile-web-app-capable" content="yes" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=860;" />
+	<meta charset="utf-8">
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=860;" />
  
-  <title>Account Deleted | {site_name}</title>
+	<title>Account Deleted | {site_name}</title>
 
-  <link rel="stylesheet" href="{stylesheet='site/boilerplate'}" type="text/css" />
-  <link rel="stylesheet" href="{stylesheet='site/standalone'}" type="text/css" />
+	<link rel="stylesheet" href="{stylesheet='site/boilerplate'}" type="text/css" />
+	<link rel="stylesheet" href="{stylesheet='site/standalone'}" type="text/css" />
 </head>
 <body class="small">
-  <div class="body">
-    <?php if (isset($_POST['reason'])) { ?>
-    <h1>Thank You!</h1>
-    <h3>Your feedback is appreciated!</h3>
-    <script type="text/javascript">
-      setTimeout("location.href = '{site_url}';", 2000);
-    </script>
-  <?php } else { ?>
-    <h1>Account Deleted</h1>  
-    <p>Could you let us know what we did wrong?</p>
-    <p>Your feedback will really help us improve the {site_name}.</p>
-    <form action="" method="post">
-     <textarea name="reason" class="input" rows="8"></textarea></td>
-      <div class="button-wrap clearfix">
-        <button type="submit" class="super green button"><span>Send</span></button>
-        <a href="/" class="super red button"><span>No Thanks</span></a>
-      </div>
-    </form>
-  <?php } ?>
-  </div>
+	<div class="body">
+		<?php if (isset($_POST['reason'])) { ?>
+		<h1>Thank You!</h1>
+		<h3>Your feedback is appreciated!</h3>
+		<script type="text/javascript">
+			setTimeout("location.href = '{site_url}';", 2000);
+		</script>
+	<?php } else { ?>
+		<h1>Account Deleted</h1>	
+		<p>Could you let us know what happened?</p>
+		<p>Your feedback will really help us improve the {site_name}.</p>
+		<form action="" method="post">
+		 <textarea name="reason" class="input" rows="8"></textarea></td>
+			<div class="button-wrap clearfix">
+				<button type="submit" class="super green button"><span>Send</span></button>
+				<a href="{site_url}" class="super red button"><span>No Thanks</span></a>
+			</div>
+		</form>
+	<?php } ?>
+	</div>
 </body>
 </html>
