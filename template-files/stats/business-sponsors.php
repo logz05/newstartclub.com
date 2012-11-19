3f71441f893a56ab4a91f9a5de8a835a02674a26
Cavell Blood <cavellblood@me.com>
2012-11-18 20:33:59 -0800

    Added template to list business sponsors

---
+++ template-files/stats/business-sponsors.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+  <meta charset="utf-8">
+	<title>Sponsor List</title>
+	<meta name="description" content="" />
+  	<meta name="keywords" content="" />
+	<meta name="robots" content="" />
+</head>
+<body>
+<pre>
+Number	Name	Address
+{exp:channel:entries channel="locations" order_by="category_id" sort="asc" status="open" search:location_type="=profit"}{categories}{category_id}{/categories}	{title}	{location_address}, {location_city}, {location_state} {location_zip}
+{/exp:channel:entries}
+</pre>
+</body>
+</html>
