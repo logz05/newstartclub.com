5b1cd125d093c9597a5fd397f37808a958dd15bf
Cavell Blood <cavellblood@me.com>
2012-11-18 20:39:28 -0800

    Recipe short URL

---
+++ template-files/recipe/index.php
@@ -0,0 +1,5 @@
+{exp:channel:entries channel="recipes" entry_id="{segment_2}"}
+
+	{exp:http_header status="301" location="{url_title_path='recipes/detail'}" terminate="yes"}
+
+{/exp:channel:entries}
