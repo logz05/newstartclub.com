39d94efdf3f8af6e7f9ba0abb67cb1514127a294
Cavell Blood <cavellblood@me.com>
2012-12-10 15:28:11 -0800

    Working on new partner application

---
+++ template-files/partners/application-submitted.php
@@ -0,0 +1,28 @@
+{embed="embeds/_doc-top" 
+	class="services"
+	title="Partnership Application"
+}
+<ul class="trail">
+	<li><a href="{path='site_index'}">Home</a></li>
+	<li><a href="{path='{segment_1}'}">Partners</a></li>
+</ul>
+<div class="heading clearfix">
+	<h1>
+		Partnership Application
+	</h1>
+</div>
+<div class="grid23 clearfix">
+	<div class="left">
+		<h1>Application Sent!</h1>
+		<p>Thank you for submitting a partnership application!</p>
+		<p>A {site_name} representative will contact you after your application has been reviewed.</p>
+		<p class="button-wrap">
+			<a href="{site_url}" class="super green button"><span>Home</span></a>
+		</p>
+	</div>
+	<div class="sidebar right">
+		<header class="bar">Partnership Application</header>
+		<p>{site_name} partners must be individuals that abide by and promote the principles of the <a href="http://newstart.com">NEWSTART&reg;</a> acronym and should be in harmony with the teachings of the Seventh-day Adventist Church. We reserve the right to deny or revoke partnership.</p>
+	</div>
+</div>
+{embed="embeds/_doc-bottom"}
--- template-files/partners/apply.php
+++ template-files/partners/apply.php
@@ -13,161 +13,157 @@
 </div>
 <div class="grid23 clearfix">
 	<div class="left">
-		<form action="/partners/application-submitted" method="post" enctype="multipart/form-data">
+		{exp:freeform:form
+			form_name="partner_application"
+			required="ff_first_name|ff_last_name"
+			return="partners/application-submitted"
+		}
 			<table>
+				{exp:user:stats dynamic="off"}
 				<tr>
-					<th scope="row">
-						<label for="name">Name</label>
-					</th>
-					<td>
-						<input type="text" name="name" class="input" id="name" size="25" value="{exp:user:stats dynamic='off'}{member_first_name} {member_last_name}{/exp:user:stats}" />
-					</td>
+					<th scope="row"><label for="ff_first_name">First Name</label></th>
+					<td><input type="text" class="input" name="ff_first_name" id="ff_first_name" value="{member_first_name}" size="20" /></td>
+				</tr>
+				<tr>
+					<th scope="row"><label for="ff_last_name">Last Name</label></th>
+					<td><input type="text" class="input" name="ff_last_name" id="ff_last_name" value="{member_last_name}" size="22" /></td>
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="email">E-mail</label>
+						<label for="partner_credentials">Credentials</label>
 					</th>
 					<td>
-						<input type="text" name="email" class="input" id="email" size="28" value="{exp:user:stats dynamic='off'}{username}{/exp:user:stats}" />
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">
-						<label for="credentials">Credentials</label>
-					</th>
-					<td>
-						<input type="text" name="credentials" class="input" id="credentials" size="8" value="" />
+						<input type="text" name="partner_credentials" class="input" id="partner_credentials" size="8" value="" />
 						<p class="instructions">i.e. MD, RN, MPH, etc.</p>
 					</td>
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="address">Business Address</label>
+						<label for="partner_specialty">Specialty</label>
 					</th>
 					<td>
-						<input type="text" name="address" class="input" id="address" size="32" value="" />
+						<input type="text" name="partner_specialty" class="input" id="partner_specialty" value="" size="23" value="" />
 					</td>
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="city">City</label>
+						<label for="ff_message">Biography</label>
 					</th>
 					<td>
-						<input type="text" name="city" class="input" id="city" value="" size="20" value="" />
+						<textarea class="input" id="ff_message" name="ff_message" rows="5" cols="28" autocomplete="off"></textarea>
 					</td>
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="state">State</label>
+						<label for="partner_image_upload">Picture</label>
+						<div></div>
 					</th>
 					<td>
-						<select name="state" class="input"> 
+						{freeform:field:partner_image_upload} 
+						<p class="instructions">Accepted file types: .jpg, .png, .gif</p>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="ff_address">Business Address</label>
+					</th>
+					<td>
+						<input type="text" name="ff_address" class="input" id="ff_address" size="32" value="" />
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="ff_city">City</label>
+					</th>
+					<td>
+						<input type="text" name="ff_city" class="input" id="ff_city" value="" size="20" value="" />
+					</td>
+				</tr>
+				<tr>
+					<th scope="row">
+						<label for="ff_state">State</label>
+					</th>
+					<td>
+						<select name="ff_state" class="input"> 
 							<option value="--">--</option>
-							<option value="Alabama">Alabama</option>
-							<option value="Alaska">Alaska</option>
-							<option value="Arizona">Arizona</option>
-							<option value="Arkansas">Arkansas</option>
-							<option value="California">California</option>
-							<option value="Colorado">Colorado</option>
-							<option value="Connecticut">Connecticut</option>
-							<option value="Delaware">Delaware</option>
-							<option value="District of Columbia">District of Columbia</option>
-							<option value="Florida">Florida</option>
-							<option value="Georgia">Georgia</option>
-							<option value="Guam">Guam</option>
-							<option value="Hawaii">Hawaii</option>
-							<option value="Idaho">Idaho</option>
-							<option value="Illinois">Illinois</option>
-							<option value="Indiana">Indiana</option>
-							<option value="Iowa">Iowa</option>
-							<option value="Kansas">Kansas</option>
-							<option value="Kentucky">Kentucky</option>
-							<option value="Louisiana">Louisiana</option>
-							<option value="Maine">Maine</option>
-							<option value="Maryland">Maryland</option>
-							<option value="Massachusetts">Massachusetts</option>
-							<option value="Michigan">Michigan</option>
-							<option value="Minnesota">Minnesota</option>
-							<option value="Mississippi">Mississippi</option>
-							<option value="Missouri">Missouri</option>
-							<option value="Montana">Montana</option>
-							<option value="Nebraska">Nebraska</option>
-							<option value="Nevada">Nevada</option>
-							<option value="New Hampshire">New Hampshire</option>
-							<option value="New Jersey">New Jersey</option>
-							<option value="New Mexico">New Mexico</option>
-							<option value="New York">New York</option>
-							<option value="North Carolina">North Carolina</option>
-							<option value="North Dakota">North Dakota</option>
-							<option value="Ohio">Ohio</option>
-							<option value="Oklahoma">Oklahoma</option>
-							<option value="Oregon">Oregon</option>
-							<option value="Pennsylvania">Pennsylvania</option>
-							<option value="Puerto Rico">Puerto Rico</option>
-							<option value="Rhode Island">Rhode Island</option>
-							<option value="South Carolina">South Carolina</option>
-							<option value="South Dakota">South Dakota</option>
-							<option value="Tennessee">Tennessee</option>
-							<option value="Texas">Texas</option>
-							<option value="Utah">Utah</option>
-							<option value="Vermont">Vermont</option>
-							<option value="Virginia">Virginia</option>
-							<option value="Virgin Islands">Virgin Islands</option>
-							<option value="Washington">Washington</option>
-							<option value="West Virginia">West Virginia</option>
-							<option value="Wisconsin">Wisconsin</option>
-							<option value="Wyoming">Wyoming</option>
+							<option value="AL">Alabama</option>
+							<option value="AK">Alaska</option>
+							<option value="AZ">Arizona</option>
+							<option value="AR">Arkansas</option>
+							<option value="CA">California</option>
+							<option value="CO">Colorado</option>
+							<option value="CT">Connecticut</option>
+							<option value="DE">Delaware</option>
+							<option value="DC">District of Columbia</option>
+							<option value="FL">Florida</option>
+							<option value="GA">Georgia</option>
+							<option value="HI">Hawaii</option>
+							<option value="ID">Idaho</option>
+							<option value="IL">Illinois</option>
+							<option value="IN">Indiana</option>
+							<option value="IA">Iowa</option>
+							<option value="KS">Kansas</option>
+							<option value="KY">Kentucky</option>
+							<option value="LA">Louisiana</option>
+							<option value="ME">Maine</option>
+							<option value="MD">Maryland</option>
+							<option value="MA">Massachusetts</option>
+							<option value="MI">Michigan</option>
+							<option value="MN">Minnesota</option>
+							<option value="MS">Mississippi</option>
+							<option value="MO">Missouri</option>
+							<option value="MT">Montana</option>
+							<option value="NE">Nebraska</option>
+							<option value="NV">Nevada</option>
+							<option value="NH">New Hampshire</option>
+							<option value="NJ">New Jersey</option>
+							<option value="NM">New Mexico</option>
+							<option value="NY">New York</option>
+							<option value="NC">North Carolina</option>
+							<option value="ND">North Dakota</option>
+							<option value="OH">Ohio</option>
+							<option value="OK">Oklahoma</option>
+							<option value="OR">Oregon</option>
+							<option value="PA">Pennsylvania</option>
+							<option value="PR">Puerto Rico</option>
+							<option value="RI">Rhode Island</option>
+							<option value="SC">South Carolina</option>
+							<option value="SD">South Dakota</option>
+							<option value="TN">Tennessee</option>
+							<option value="TX">Texas</option>
+							<option value="UT">Utah</option>
+							<option value="VT">Vermont</option>
+							<option value="VA">Virginia</option>
+							<option value="VI">Virgin Islands</option>
+							<option value="WA">Washington</option>
+							<option value="WV">West Virginia</option>
+							<option value="WI">Wisconsin</option>
+							<option value="WY">Wyoming</option>
 						</select>
 					</td>
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="zip">Zip Code</label>
+						<label for="ff_zip_code">Zip Code</label>
 					</th>
 					<td>
-						<input type="text" name="zip" class="input" id="zip" value="" size="7" value="" />
+						<input type="text" name="ff_zip_code" class="input" id="ff_zip_code" value="" size="7" value="" />
 					</td>
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="phone">Business Phone</label>
+						<label for="ff_phone">Business Phone</label>
 					</th>
 					<td>
-						<input type="text" name="phone" class="input" id="phone" value="" size="15" value="" />
+						<input type="text" name="ff_phone" class="input" id="ff_phone" value="" size="15" value="" />
 					</td>
 				</tr>
 				<tr>
 					<th scope="row">
-						<label for="specialty">Specialty</label>
+						<label for="ff_affiliation">Local Church Affiliation</label>
 					</th>
 					<td>
-						<input type="text" name="specialty" class="input" id="specialty" value="" size="23" value="" />
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">
-						<label for="bio">Biography</label>
-					</th>
-					<td>
-						<textarea class="input" id="bio" name="bio" rows="5" cols="28" autocomplete="off"></textarea>
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">
-						<label for="partnerPicture">Picture</label>
-						<div></div>
-					</th>
-					<td>
-						<input type="file" name="partnerPicture" id="partnerPicture" />
-						<p class="instructions">Accepted file types: .jpg, .bmp, .tiff, .png</p>
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">
-						<label for="affiliation">Local Church Affiliation</label>
-					</th>
-					<td>
-						<input type="text" name="affiliation" class="input" id="affiliation" size="20" value="" />
+						<input type="text" name="ff_affiliation" class="input" id="ff_affiliation" size="20" value="" />
 					</td>
 				</tr>
 				<tr>
@@ -176,11 +172,13 @@
 					<td>
 						<div class="button-wrap">
 							<button type="submit" class="super green button"><span>Submit</span></button>
+							<input type="text" class="input hidden" name="ff_sender_email" id="ff_sender_email" value="{email}" size="28" />
 						</div>
 					</td>
 				</tr>
+				{/exp:user:stats}
 			</table>
-		</form>
+		{/exp:freeform:form}
 	</div>
 	<div class="sidebar right">
 		<header class="bar">Partnership Application</header>
